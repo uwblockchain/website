e82f4bba6b811b9e455f8581aa2e59bf14771e8b
kirakeld <56563509+alexdzius@users.noreply.github.com>
2021-10-31 15:12:27 -0700

    add

--- blockchain-society-website/src/components/home/Events/Events.tsx
+++ blockchain-society-website/src/components/home/Events/Events.tsx
@@ -1,12 +1,25 @@
 import { Container, Row, Col } from "react-bootstrap";
 import TUE from "../../../images/icons/TUE.png";
 import THU from "../../../images/icons/THU.png";
+import AOS from "aos";
+import { useEffect } from "react";
 
 function Events() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="p-6">
+    <div 
+      className="p-6"
+      data-aos="fade-up"
+      data-aos-duration="500"
+    >
       <Container>
-        <span className="text-4xl">Our Programs</span>
+        <span 
+        className="text-4xl"
+        data-aos="fade-up"
+        data-aos-duration="550"
+        >Our Programs</span>
         <Row className="flex justify-center content-center bg-yellow px-16">
           <Col className="py-4">
             <img src={TUE} alt="TUE-icon" className="block m-auto w-16 mb-4" />
