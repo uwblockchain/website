29606f0393a239cd12cfa99eae2388b79005793b
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-11-07 11:54:34 -0800

    Commit changes

--- blockchain-society-website/src/components/home/Events/Events.tsx
+++ blockchain-society-website/src/components/home/Events/Events.tsx
@@ -1,15 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
-import TUE from "../../../images/icons/TUE.png";
-import THU from "../../../images/icons/THU.png";
+import tuesday from "../../../images/icons/TUE.png";
+import thursday from "../../../images/icons/THU.png";
+import AOS from "aos";
+import { useEffect } from "react";
 
 function Events() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
   return (
-    <div className="p-6">
+    <div 
+      className="p-6"
+      data-aos="fade-up"
+      data-aos-duration="500"
+    >
       <Container>
-        <span className="text-4xl">Our Programs</span>
+        <span 
+        className="text-4xl"
+        data-aos="fade-up"
+        data-aos-duration="550"
+        >Our Programs</span>
         <Row className="flex justify-center content-center bg-yellow px-16">
           <Col className="py-4">
-            <img src={TUE} alt="TUE-icon" className="block m-auto w-16 mb-4" />
+            <img src={tuesday} alt="tuesday-icon" className="block m-auto w-16 mb-4" />
             <p className="text-black-600 font-bold text-3xl">Meeting</p>
             <div className="leading-3 font-bold">
               <p>Weekly on Tuesday</p>
@@ -18,7 +32,7 @@ function Events() {
             </div>
           </Col>
           <Col className="py-4">
-            <img src={THU} alt="THU-icon" className="block m-auto w-16 mb-4" />
+            <img src={thursday} alt="thursday-icon" className="block m-auto w-16 mb-4" />
             <p className="text-black-600 font-bold text-3xl">Meeting</p>
             <div className="leading-3 font-bold">
               <p>Weekly on Thursday</p>
--- blockchain-society-website/src/components/home/Upcoming/Upcoming.tsx
+++ blockchain-society-website/src/components/home/Upcoming/Upcoming.tsx
@@ -1,9 +1,18 @@
 import { Container, Row } from "react-bootstrap";
 import Event from "./Event";
+import AOS from "aos";
+import { useEffect } from "react";
 
 function Upcoming() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="text-left pt-50%">
+    <div 
+    className="text-left pt-50%"
+    data-aos="fade-right"
+    data-aos-duration="550"
+    >
       <Container>
           <Row>
               <Event title="Reprogram The World With Conflux Blockchain Workshop" type="Workshop" time="4:00 pm PST" month="OCT" day="25"></Event>
