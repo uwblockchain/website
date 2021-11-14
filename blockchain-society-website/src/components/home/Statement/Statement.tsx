b7e6d38f9ac7acbe45b43b8830f0cd76d6e3861a
Han Pham <hanpham@Hans-MacBook-Pro.local>
2021-11-14 12:59:16 -0800

    Turned into component

---
+++ blockchain-society-website/src/components/home/Statement/Statement.tsx
@@ -0,0 +1,12 @@
+function Statement() {
+  return (
+    <div className="bg-gradient-to-b from-black via-purple-900 to-indigo-900 h-96 w-screen flex items-center justify-center">
+      <h2 className="text-white italic font-semibold">
+        Our mission is our north star, it should determine our pillars and
+        rarely change
+      </h2>
+    </div>
+  );
+}
+
+export default Statement;
