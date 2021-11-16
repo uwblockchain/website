911705e001a285adcee6d3ced4740de59ee51cd7
Philip <pvu2002@outlook.com>
2021-11-15 23:23:08 -0800

    Wednesday's Requests for Sunday
    - moved about us below bar
    - added animations
    - turned text to black, background to white

--- blockchain-society-website/src/components/home/About/About.tsx
+++ blockchain-society-website/src/components/home/About/About.tsx
@@ -1,26 +1,32 @@
 import art0 from "../../../images/stock/uwbsart1.png";
 import bg_uwquad from "../../../images/stock/uw_quad.jpg";
-
+import AOS from "aos";
+import { useEffect } from "react";
 function About() {
   return (
     <div
-      className="w-screen text-4xl bg-indigo-900 align-middle pb-10"
+      className="w-screen text-4xl bg-white align-middle pb-10"
       id="about"
     >
       <div className="relative overflow-hidden flex justify-center items-center">
-        <h3 className="z-10 absolute w-full text-4xl text-white text-center font-semibold">
-          About Us
-        </h3>
         <img
           alt="uw_quad"
-          className="h-36 lg:h-48 w-full opacity-25 object-cover"
+          className="h-36 lg:h-48 w-full opacity-80 object-cover"
           src={bg_uwquad}
         ></img>
+
+      </div>
+      <div
+        data-aos="fade-up"
+        data-aos-duration="550">
+        <h3 className="pt-3 z-10 w-full text-4xl text-black text-center">
+          About Us
+        </h3>
       </div>
       <div className="pt-8 lg:grid lg:grid-cols-2 lg:pl-36 ">
-        <div className="flex justify-center">
-          <div className="text-center text-white px-4 lg:w-3/4 content-center">
-            <h3 className="text-2xl py-8 text-left font-semibold">
+        <div className="flex justify-center" data-aos="fade-right" data-aos-duration="550">
+          <div className="text-center text-black px-4 lg:w-3/4 content-center">
+            <h3 className="text-2xl py-8 pt+4 text-left font-semibold">
               A tri-campus organization.
             </h3>
             <p className="text-lg text-left leading-loose">
@@ -32,7 +38,7 @@ function About() {
             </p>
           </div>
         </div>
-        <div className="flex justify-center lg:pr-36">
+        <div className="flex justify-center lg:pr-36" data-aos="fade-left" data-aos-duration="550">
           <img src={art0} alt="art0" className="w-80"></img>
         </div>
       </div>
